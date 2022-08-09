bee038051a7caee7eae0a735e9be5f1ec7eb3878
Raya Abdullah <108306624+rayabdullah1@users.noreply.github.com>
2022-08-09 04:33:07 +0300

    Add files via upload

---
+++ getv.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head> 
+        <meta charset="UTF-8">
+        <title>GET NUMBER</title>
+    </head>
+
+    <style> 
+body {text-align: center;}
+    </style>
+
+<body background="image4.jpg">
+    <h1 style="color: white;">GET SENSOR VALUE FROM THE USER</h1> 
+    
+    <form action="conn.php" method="GET">
+
+    <h4>Enter the number:</h4> <input type="number" step="1" pattern="^[-/d]/d*$" name="valueR" required>
+    <input type="submit" name="send" value="send">
+    
+    </form>
+
+</body>
+
+</html>
